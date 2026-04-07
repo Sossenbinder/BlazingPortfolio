23ca671f6c574f5237a82d12e33eb14ab691f125
Sossenbinder <Stefan.Daniel.Schranz96@gmail.com>
2026-04-07 22:04:10 +0200

    Upgrade to net10 + trim

--- BlazingPortfolio/Pages/Home.razor.cs
+++ BlazingPortfolio/Pages/Home.razor.cs
@@ -1,44 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.JSInterop;
-
 namespace BlazingPortfolio.Pages
 {
-
 	public partial class Home
 	{
-		[Inject]
-		public IJSRuntime JsRuntime { get; set; }
-
-		[Inject]
-		public IHttpClientFactory HttpClientFactory { get; set; }
-		
-		[Inject]
-		private IWebAssemblyHostEnvironment HostEnvironment { get; set; }
-
-		private bool _flipped;
-
 		private int Age => (int)((DateTime.UtcNow - DateTime.Parse("1996-06-06")).TotalDays / 365.2425);
-		
+
 		private int YOE => (int)((DateTime.UtcNow - DateTime.Parse("2017-10-01")).TotalDays / 365.2425);
-
-		protected override async Task OnAfterRenderAsync(bool firstRender)
-		{
-			var client = HttpClientFactory.CreateClient();
-
-			if (_flipped || HostEnvironment.IsDevelopment())
-			{
-				return;
-			}
-
-			try
-			{
-				await client.PostAsync("https://blazingportfolio.azurewebsites.net/api/AddVisit", null);
-			}
-			catch (Exception)
-			{
-				// Just swallow this exception. It's not horrible if a visit is not registered
-			}
-		}
 	}
 }
--- BlazingPortfolio/Program.cs
+++ BlazingPortfolio/Program.cs
@@ -1,22 +1,8 @@
 using BlazingPortfolio;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
-
-builder.Services.AddHttpClient();
-builder.Services.RemoveAll<IHttpMessageHandlerBuilderFilter>();
-builder.Services.AddBlazorise( options =>
-    {
-        options.Immediate = true;
-    } )
-    .AddBootstrapProviders()
-    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
